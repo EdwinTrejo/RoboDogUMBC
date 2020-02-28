0225cea98134c2f8e84062bfc7976e82bd14b1db
EdwinTrejo <edwintrejo@outlook.com>
2020-02-28 17:26:35 -0500

    Added some pseudo code for working with the servos

--- testDog/servo/servo.ino
+++ testDog/servo/servo.ino
@@ -54,49 +54,48 @@ void setup() {
   delay(10);
 }
 
+//can only go up to 180
+void setPWMDeg(uint8_t port, int deg){ 
+  uint16_t actualRate = ((450/180) * (uint16_t)deg) + 149;
+  actualRate = (actualRate < 150) ? 150 : actualRate;
+  pwm.setPWM(port, 0, actualRate);
+}
+
 // You can use this function if you'd like to set the pulse length in seconds
 // e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert input seconds to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
-}
+//void setServoPulse(uint8_t n, double pulse) {
+//  double pulselength;
+//  
+//  pulselength = 1000000;   // 1,000,000 us per second
+//  pulselength /= SERVO_FREQ;   // Analog servos run at ~60 Hz updates
+//  Serial.print(pulselength); Serial.println(" us per period"); 
+//  pulselength /= 4096;  // 12 bits of resolution
+//  Serial.print(pulselength); Serial.println(" us per bit"); 
+//  pulse *= 1000000;  // convert input seconds to us
+//  pulse /= pulselength;
+//  Serial.println(pulse);
+//  pwm.setPWM(n, 0, pulse);
+//}
 
 void loop() {
   // Drive each servo one at a time using setPWM()
-  Serial.println(servonum);
-  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
-    pwm.setPWM(servonum, 0, pulselen);
-  }
-
-  delay(500);
-  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
-    pwm.setPWM(servonum, 0, pulselen);
-  }
-
-  delay(500);
-
-  // Drive each servo one at a time using writeMicroseconds(), it's not precise due to calculation rounding!
-  // The writeMicroseconds() function is used to mimic the Arduino Servo library writeMicroseconds() behavior. 
-  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(500);
-  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
-
-  delay(500);
-
-  servonum++;
-  if (servonum > 7) servonum = 0; // Testing the first 8 servo channels
+  //for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+    setPWMDeg(0, 0);
+    setPWMDeg(3, 0);
+    delay(1000);
+    //delay(1000);
+    //setPWMDeg(0, 30);
+    //setPWMDeg(3, 30);
+    //delay(1000);
+    //setPWMDeg(0, 90);
+    //setPWMDeg(3, 90);
+    //delay(1000);
+    //setPWMDeg(0, 150);
+    //setPWMDeg(3, 150);
+    //delay(1000);
+    setPWMDeg(0, 174);
+    setPWMDeg(3, 174);
+    delay(1000);
+//    /Serial.println(pulselen);
+  //}
 }
